06fd4694c1d82791fbf93a635c80b02552125e3e
dvenkat285 <116060501+dvenkat285@users.noreply.github.com>
2024-02-24 20:34:13 +0530

    Add project files.

---
+++ WebAPI/Controllers/EmployeeDataController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeDataController : ControllerBase
+    {
+        public string[] myEmployee = { "Adil", "Bilal", "Zain" };
+
+        [HttpGet]
+        public string[] GetEmployee()
+        {
+            return myEmployee;
+        }
+
+
+        [HttpGet("{id}")]
+        public string GetEmployeeByIndex(int id)
+        {
+            return myEmployee[id];
+        }
+
+
+
+    }
+}
